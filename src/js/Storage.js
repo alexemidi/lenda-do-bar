d767531225d0789e4ec94f0011aef5228b61fd2d
alexemidi <almeidaalex.cs@gmail.com>
2026-01-09 19:54:31 -0300

    Delete src directory

--- src/js/Storage.js
+++
@@ -1,4 +0,0 @@
-export const Storage = {
-    get(key, def) { try { return JSON.parse(localStorage.getItem('mentiroso_' + key)) || def; } catch { return def; } },
-    set(key, val) { localStorage.setItem('mentiroso_' + key, JSON.stringify(val)); }
-};
